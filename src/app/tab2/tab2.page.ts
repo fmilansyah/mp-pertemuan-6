321f85b781df60dcacd0d5fac0eacccbe9d28994
Febri Milansyah <febrimilan00@gmail.com>
2021-11-01 11:39:16 +0700

    Tugas

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  public num1: number;
+  public num2: number;
+  public num3: number;
+
   constructor() {}
+
+  result() {
+    if (this.num1 == null || this.num2 == null) {
+      alert('Ada inputan yang kosong');
+      this.num1 = null;
+      this.num2 = null;
+      this.num3 = null;
+    } else {
+      this.num3 = this.num1 * this.num2;
+    }
+  }
 
 }
